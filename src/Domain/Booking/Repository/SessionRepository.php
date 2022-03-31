eea25ae59418456ac1e931896b3c8d9dfc2ccc95
Oleg Sviridov <oleg.sviridov@resolventagroup.com>
2022-03-31 17:50:49 +0200

    Remove not important attributes

--- src/Domain/Booking/Repository/SessionRepository.php
+++ src/Domain/Booking/Repository/SessionRepository.php
@@ -6,12 +6,6 @@ use App\Domain\Booking\Entity\Session;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
-/**
- * @method Session|null find($id, $lockMode = null, $lockVersion = null)
- * @method Session|null findOneBy(array $criteria, array $orderBy = null)
- * @method array<Session> findAll()
- * @method array<Session> findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
- */
 final class SessionRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)

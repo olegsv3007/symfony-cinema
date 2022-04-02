607601833e25e64bc4908649ce74352cebe8cf35
Oleg Sviridov <oleg.sviridov@resolventagroup.com>
2022-04-02 11:54:47 +0200

    Create action for booking ticket

--- src/Controller/BookingController.php
+++ src/Controller/BookingController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Domain\Booking\Entity\Session;
+use App\Domain\Booking\Entity\TransferObject\BookTicketDTOFactory;
 use App\Domain\Booking\Repository\SessionRepository;
+use App\Domain\Booking\Repository\TicketRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,8 +22,12 @@ final class BookingController extends AbstractController
     }
 
     /** @Route("/session/{id}/book", name="app.book", methods={"POST"}) */
-    public function book(): Response
+    public function book(Request $request, Session $session, TicketRepository $ticketRepository): Response
     {
+        $clientInfo = BookTicketDTOFactory::createFromRequest($request);
+        $ticket = $session->bookTicket($clientInfo);
+        $ticketRepository->add($ticket);
+
         return $this->redirectToRoute('app.main');
     }
 }

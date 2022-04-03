029e9c8e909a4ed7735dc1344e890aee89ce468c
Oleg Sviridov <oleg.sviridov@resolventagroup.com>
2022-04-03 19:12:47 +0200

    Change variable in route session/id/book

--- src/Controller/BookingController.php
+++ src/Controller/BookingController.php
@@ -2,14 +2,14 @@
 
 namespace App\Controller;
 
-use App\Domain\Booking\Entity\Session;
+use App\Domain\Booking\Command\BookTicketCommand;
 use App\Domain\Booking\Entity\TransferObject\BookTicketDTOFactory;
-use App\Domain\Booking\Exception\TicketsAreOverException;
 use App\Domain\Booking\Repository\SessionRepository;
-use App\Domain\Booking\Repository\TicketRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\Exception\HandlerFailedException;
+use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 final class BookingController extends AbstractController
@@ -22,16 +22,16 @@ final class BookingController extends AbstractController
         return $this->render('main.html.twig', ['sessions' => $sessions]);
     }
 
-    /** @Route("/session/{id}/book", name="app.book", methods={"POST"}) */
-    public function book(Request $request, Session $session, TicketRepository $ticketRepository): Response
+    /** @Route("/session/{sessionId}/book", name="app.book", methods={"POST"}) */
+    public function book(Request $request, int $sessionId, MessageBusInterface $bus): Response
     {
         $clientInfo = BookTicketDTOFactory::createFromRequest($request);
 
         try {
-            $ticket = $session->bookTicket($clientInfo);
-            $ticketRepository->add($ticket);
-        } catch (TicketsAreOverException $e) {
-            $this->addFlash('notice', $e->getMessage());
+            $command = new BookTicketCommand($sessionId, $clientInfo);
+            $bus->dispatch($command);
+        } catch (HandlerFailedException $e) {
+            $this->addFlash('notice', $e->getPrevious()->getMessage());
         }
 
         return $this->redirectToRoute('app.main');

380c6e49db4e348aa401ca4ec63dc97deaf11e20
Oleg Sviridov <oleg.sviridov@resolventagroup.com>
2022-04-04 17:41:09 +0200

    Check free tickets without exception

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -3,33 +3,34 @@
 namespace App\Controller;
 
 use App\Domain\Booking\Command\BookTicketCommand;
+use App\Domain\Booking\Entity\Session;
 use App\Domain\Booking\Entity\TransferObject\BookTicketDTOFactory;
-use App\Domain\Booking\Exception\TicketsAreOverException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Messenger\Exception\HandlerFailedException;
 use Symfony\Component\Messenger\Exception\ValidationFailedException;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 final class SessionController extends AbstractController
 {
-    /** @Route("/session/{sessionId}/book", name="session.book", methods={"POST"}) */
-    public function book(Request $request, int $sessionId, MessageBusInterface $bus): Response
+    /** @Route("/session/{session}/book", name="session.book", methods={"POST"}) */
+    public function book(Request $request, Session $session, MessageBusInterface $bus): Response
     {
+        if (!$session->hasFreeTickets()) {
+            $this->addFlash('error', 'Билеты кончились');
+
+            return $this->redirectToRoute('home');
+        }
+
         $clientInfo = BookTicketDTOFactory::createFromRequest($request);
 
         try {
-            $command = new BookTicketCommand($sessionId, $clientInfo);
+            $command = new BookTicketCommand($session->getId()->getId(), $clientInfo);
             $bus->dispatch($command);
         } catch (ValidationFailedException $e) {
             foreach ($e->getViolations() as $violation) {
                 $this->addFlash('error', $violation->getMessage());
-            }
-        } catch (HandlerFailedException $e) {
-            if ($e->getPrevious() instanceof TicketsAreOverException) {
-                $this->addFlash('error', $e->getPrevious()->getMessage());
             }
         }
 

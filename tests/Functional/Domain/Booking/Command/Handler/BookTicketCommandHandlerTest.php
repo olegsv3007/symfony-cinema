99b25d912ce1b3b07a5fa07cc7ba9bf83aa6d951
Oleg Sviridov <oleg.sviridov@resolventagroup.com>
2022-04-11 10:01:22 +0200

    Separate tests for handle and validate BookTicketCommand

---
+++ tests/Functional/Domain/Booking/Command/Handler/BookTicketCommandHandlerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Tests\Functional\Domain\Booking\Command\Handler;
+
+use App\Domain\Booking\Command\BookTicketCommand;
+use App\Tests\Functional\FunctionalKernelTestCase;
+use Symfony\Component\Messenger\Exception\HandlerFailedException;
+use Symfony\Component\Uid\Uuid;
+
+final class BookTicketCommandHandlerTest extends FunctionalKernelTestCase
+{
+    public function testCorrectCommandCanBeHandleForSessionWithFreeTickets(): void
+    {
+        $session = $this->getSessionWithFreeTickets();
+        $command = $this->createNewCommand($session->getId());
+
+        $this->bus->dispatch($command);
+
+        $this->assertCount(1, $session->getTickets());
+    }
+
+    public function testCorrectCommandThrowExceptionForSessionWithoutFreeTickets(): void
+    {
+        $session = $this->getSessionWithoutFreeTickets();
+        $command = $this->createNewCommand($session->getId());
+
+        $this->expectException(HandlerFailedException::class);
+        $this->expectExceptionMessage('Билеты кончились');
+
+        $this->bus->dispatch($command);
+    }
+
+    private function createNewCommand(Uuid $sessionId): BookTicketCommand
+    {
+        return new BookTicketCommand($sessionId, 'testClient', '9876543210');
+    }
+}
--- tests/Functional/FunctionalKernelTestCase.php
+++ tests/Functional/FunctionalKernelTestCase.php
@@ -2,17 +2,22 @@
 
 namespace App\Tests\Functional;
 
+use App\Domain\Booking\Entity\Session;
 use App\Domain\Booking\Repository\SessionRepository;
+use App\Tests\DataFixtures\SessionWithFreeTicketsFixture;
+use App\Tests\DataFixtures\SessionWithoutFreeTicketsFixture;
 use Liip\TestFixturesBundle\Services\DatabaseToolCollection;
 use Liip\TestFixturesBundle\Services\DatabaseTools\AbstractDatabaseTool;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 abstract class FunctionalKernelTestCase extends KernelTestCase
 {
     protected SessionRepository $sessionRepository;
     protected MessageBusInterface $bus;
     protected AbstractDatabaseTool $databaseTool;
+    protected ValidatorInterface $validator;
 
     protected function setUp(): void
     {
@@ -23,5 +28,24 @@ abstract class FunctionalKernelTestCase extends KernelTestCase
         $this->sessionRepository = $container->get(SessionRepository::class);
         $this->bus = $container->get(MessageBusInterface::class);
         $this->databaseTool = $container->get(DatabaseToolCollection::class)->get();
+        $this->validator = $container->get(ValidatorInterface::class);
+    }
+
+    protected function getSessionWithFreeTickets(): Session
+    {
+        $this->databaseTool->loadFixtures([
+            SessionWithFreeTicketsFixture::class,
+        ]);
+
+        return $this->sessionRepository->findAll()[0];
+    }
+
+    protected function getSessionWithoutFreeTickets(): Session
+    {
+        $this->databaseTool->loadFixtures([
+            SessionWithoutFreeTicketsFixture::class,
+        ]);
+
+        return $this->sessionRepository->findAll()[0];
     }
 }
